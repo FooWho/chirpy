2fa554bc785fcaff84f747a4d4acfdc5be7eac63
Jason Elison <jason.elison@gmail.com>
2026-08-13 17:21:58 -0400

    C2L4

--- main.go
+++ main.go
@@ -2,10 +2,56 @@ package main
 
 import (
 	"net/http"
+	"strconv"
+	"sync/atomic"
 )
 
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (apc *apiConfig) GetHits() int {
+	return int(apc.fileserverHits.Load())
+}
+
+func (apc *apiConfig) Reset() {
+	apc.fileserverHits = atomic.Int32{}
+}
+
+func (apc *apiConfig) IncrementHits() {
+	apc.fileserverHits.Add(int32(1))
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.IncrementHits()
+		next.ServeHTTP(w, r)
+	})
+}
+
 func main() {
+	myCfg := &apiConfig{fileserverHits: atomic.Int32{}}
+
 	mux := http.NewServeMux()
-	s := http.Server{Addr: ":8080", Handler: mux}
+	mux.Handle("/app/",
+		myCfg.middlewareMetricsInc(
+			http.StripPrefix("/app/", http.FileServer(http.Dir(".")))))
+
+	mux.HandleFunc("GET /healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(200)
+		w.Write([]byte("OK"))
+	})
+
+	mux.HandleFunc("GET /metrics", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(200)
+		hits := strconv.Itoa(myCfg.GetHits())
+		w.Write([]byte("Hits: " + hits))
+	})
+
+	mux.HandleFunc("POST /reset", func(w http.ResponseWriter, r *http.Request) {
+		myCfg.Reset()
+	})
+
+	s := &http.Server{Addr: ":8080", Handler: mux}
 	s.ListenAndServe()
 }
