057914ea0630fdc492b0c2c421d56dcd0ffe6654
Jason Elison <jason.elison@gmail.com>
2026-08-24 14:54:41 -0400

    C5L6

--- main.go
+++ main.go
@@ -1,16 +1,27 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"strings"
 	"sync/atomic"
+	"time"
+
+	"github.com/FooWho/chirpy/internal/database"
+	"github.com/google/uuid"
+	"github.com/joho/godotenv"
+
+	_ "github.com/lib/pq"
 )
 
 type apiConfig struct {
 	fileserverHits atomic.Int32
+	dbQueries      *database.Queries
+	platform       string
 }
 
 func (apc *apiConfig) GetHits() int {
@@ -32,8 +43,30 @@ func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 	})
 }
 
+type cleanedBody struct {
+	CleanedBody string `json:"cleaned_body"`
+}
+
+type sUser struct {
+	ID        uuid.UUID `json:"id"`
+	CreatedAt time.Time `json:"created_at`
+	UpdatedAt time.Time `json:"updated_at`
+	Email     string    `json:"email"`
+}
+
 func main() {
-	myCfg := &apiConfig{fileserverHits: atomic.Int32{}}
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	dbURL := os.Getenv("DB_URL")
+	platform := os.Getenv("PLATFORM")
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatal("Failed to obtain database\n")
+	}
+	dbQueries := database.New(db)
+	myCfg := &apiConfig{fileserverHits: atomic.Int32{}, dbQueries: dbQueries, platform: platform}
 
 	mux := http.NewServeMux()
 	mux.Handle("/app/",
@@ -45,39 +78,45 @@ func main() {
 		w.Write([]byte("OK"))
 	})
 
-	mux.HandleFunc("GET /admin/metrics", func(w http.ResponseWriter, r *http.Request) {
-		header := w.Header()
-		header.Set("Content-Type", "text/html")
-		w.WriteHeader(200)
-		hits := myCfg.GetHits()
-		hitString := fmt.Sprintf("<html>\n  <body>\n    <h1>Welcome, Chirpy Admin!</h1>\n    <p>Chirpy has been visited %d times!</p>\n  </body>\n</html>\n", hits)
-		w.Write([]byte(hitString))
-	})
+	mux.HandleFunc("GET /admin/metrics", myCfg.getMetrics)
+	/*
+		mux.HandleFunc("GET /admin/metrics", func(w http.ResponseWriter, r *http.Request) {
+			header := w.Header()
+			header.Set("Content-Type", "text/html")
+			w.WriteHeader(200)
+			hits := myCfg.GetHits()
+			hitString := fmt.Sprintf("<html>\n  <body>\n    <h1>Welcome, Chirpy Admin!</h1>\n    <p>Chirpy has been visited %d times!</p>\n  </body>\n</html>\n", hits)
+			w.Write([]byte(hitString))
+		})
+	*/
 
 	mux.HandleFunc("POST /admin/reset", func(w http.ResponseWriter, r *http.Request) {
 		myCfg.Reset()
 	})
+	mux.HandleFunc("POST /api/validate_chirp", validateChirp)
+	/*
+		mux.HandleFunc("POST /api/validate_chirp", func(w http.ResponseWriter, r *http.Request) {
+			type parameters struct {
+				Body string `json:"body"`
+			}
 
-	mux.HandleFunc("POST /api/validate_chirp", func(w http.ResponseWriter, r *http.Request) {
-		type parameters struct {
-			Body string `json:"body"`
-		}
-
-		decoder := json.NewDecoder(r.Body)
-		params := parameters{}
-		err := decoder.Decode(&params)
-		if err != nil {
-			log.Printf("Error decoding parameters: %s", err)
-			respondWithError(w, 500, "Error decoding parameters")
-			return
-		}
-		if len(params.Body) <= 140 {
-			params.Body = replaceBadWords(params.Body)
-			respondWithJSON(w, 200, params.Body)
-		} else {
-			respondWithError(w, 400, "Chirp too long")
-		}
-	})
+			decoder := json.NewDecoder(r.Body)
+			params := parameters{}
+			err := decoder.Decode(&params)
+			if err != nil {
+				log.Printf("Error decoding parameters: %s", err)
+				respondWithError(w, 500, "Error decoding parameters")
+				return
+			}
+			if len(params.Body) <= 140 {
+				params.Body = replaceBadWords(params.Body)
+				respondWithJSON(w, 200, params.Body)
+			} else {
+				respondWithError(w, 400, "Chirp too long")
+			}
+		})
+	*/
+	mux.HandleFunc("POST /api/users", myCfg.createUser)
 
 	s := &http.Server{Addr: ":8080", Handler: mux}
 	s.ListenAndServe()
@@ -103,25 +142,13 @@ func respondWithError(w http.ResponseWriter, code int, msg string) {
 }
 
 func respondWithJSON(w http.ResponseWriter, code int, payload any) {
-	type returnVals struct {
-		CleanedBody string `json:"cleaned_body"`
-	}
-	respBody := returnVals{}
-	cleanedBody, ok := payload.(string)
-	if !ok {
-		fmt.Printf("blah")
-	} else {
-		respBody = returnVals{
-			CleanedBody: cleanedBody,
-		}
-	}
-
-	dat, err := json.Marshal(respBody)
+	dat, err := json.Marshal(payload)
 	if err != nil {
 		log.Printf("Error marshalling JSON: %s", err)
 		respondWithError(w, 500, "Error marshalling json")
 		return
 	}
+
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(code)
 	w.Write(dat)
@@ -138,4 +165,62 @@ func replaceBadWords(s string) string {
 	}
 	cleanS := strings.Join(words, " ")
 	return cleanS
+}
+
+func validateChirp(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
+		Body string `json:"body"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		log.Printf("Error decoding parameters: %s", err)
+		respondWithError(w, 500, "Error decoding parameters")
+		return
+	}
+	if len(params.Body) <= 140 {
+		cleanedBody := cleanedBody{}
+		cleanedBody.CleanedBody = replaceBadWords(params.Body)
+		respondWithJSON(w, 200, cleanedBody)
+	} else {
+		respondWithError(w, 400, "Chirp too long")
+	}
+}
+
+func (cfg *apiConfig) createUser(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
+		Email string `json:"email"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		log.Printf("Error decoding parameters: %s", err)
+		respondWithError(w, 500, params.Email)
+		return
+	}
+	user, err := cfg.dbQueries.CreateUser(r.Context(), params.Email)
+	if err != nil {
+		log.Printf("Error creating user: %s", err)
+		respondWithError(w, 500, params.Email)
+		return
+	}
+	respondWithJSON(w, 200, user)
+}
+
+func (cfg *apiConfig) getMetrics(w http.ResponseWriter, r *http.Request) {
+	header := w.Header()
+	header.Set("Content-Type", "text/html")
+	w.WriteHeader(200)
+	hits := cfg.GetHits()
+	hitString := "<html>\n"
+	hitString += "    <body>\n"
+	hitString += "        <h1>Welcome, Chirpy Admin!</h1>\n"
+	hitString += fmt.Sprintf("        <p>Chirpy has been visited %d times!</p>\n", hits)
+	hitString += "    </body>\n"
+	hitString += "</html>\n"
+	w.Write([]byte(hitString))
 }
